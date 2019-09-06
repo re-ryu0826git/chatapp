988a433f30838b5209ba03b5b178db8896016231
”TanakaRyunosuke” <ryu0826tanaka@gmail.com>
2019-09-06 15:52:59 +0000

    Edit navbar

--- resources/views/layouts/comment.blade.php
+++ resources/views/layouts/comment.blade.php
@@ -41,19 +41,31 @@
               <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav mr-auto">　
                   <li class="nav-item">
-                    <a class="nav-link" href="#">チャット一覧</a>
+                    <a class="nav-link" href="{{ action('GroupController@index') }}">チャット一覧</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">チャットグループ作成</a>
+                    <a class="nav-link" href="{{ action('GroupController@add') }}">チャットグループ作成</a>
                   </li>
                 </ul>
 
                 <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">SignOut</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
+                  <li class="nav-item dropdown">
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                          {{ Auth::user()->name }} <span class="caret"></span>
+                      </a>
+                  
+                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="{{ route('logout') }}"
+                             onclick="event.preventDefault();
+                                           document.getElementById('logout-form').submit();">
+                              {{ __('Logout') }}
+                          </a>
+                  
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              @csrf
+                          </form>
+                          
+                      </div>
                   </li>
                 </ul>
               </div>
@@ -68,3 +80,21 @@
         </div>
     </body>
 </html>
+
+
+                <ul class="navbar-nav">
+                  <li class="nav-item dropdown">
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                          {{ Auth::user()->name }} <span class="caret"></span>
+                      </a>
+                  
+                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="{{ route('logout') }}"
+                             onclick="event.preventDefault();
+                                           document.getElementById('logout-form').submit();">
+                              {{ __('Logout') }}
+                          </a>
+                  
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              @csrf
+                          </form>
